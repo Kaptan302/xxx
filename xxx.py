850978a2a5467605388421b26728af1afbae8b57
Kaptan302 <124124982+Kaptan302@users.noreply.github.com>
2023-02-01 23:07:09 +0500

    Update xxx.py
    rm -rf xxx
    git clone https://github.com/Kaptan302/xxx.git
    ls
    cd xxx
    ls
    python xxx.py

--- xxx.py
+++ xxx.py
@@ -486,21 +486,21 @@ def free1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header = ({'authority': 'free.facebook.com',
-            'method': 'GET',
-            'scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
+            header = ({"authority": 'free.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            "accept-language": 'en-US,en;q=0.9',
+            "cache-control": 'max-age=0',
+            "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+            "sec-ch-ua-mobile": '?0',
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": 'document',
+            "sec-fetch-mode": 'navigate',
+            "sec-fetch-site": 'none',
+            "sec-fetch-user": '?1',
+            "upgrade-insecure-requests": '1',
+            "user-agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
             lo = session.post('https://free.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',cookies=cookies,headers=headers).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -600,21 +600,35 @@ def free1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header = ({'authority': 'free.facebook.com',
-            'method': 'GET',
-            'scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
+            header = ({"authority": 'free.facebook.com',
+
+            "method": 'GET',
+
+            "scheme": 'https',
+
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+            "accept-language": 'en-US,en;q=0.9',
+
+            "cache-control": 'max-age=0',
+
+            "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+
+            "sec-ch-ua-mobile": '?0',
+
+            "sec-ch-ua-platform": '"Windows"',
+
+            "sec-fetch-dest": 'document',
+
+            "sec-fetch-mode": 'navigate',
+
+            "sec-fetch-site": 'none',
+
+            "sec-fetch-user": '?1',
+
+            "upgrade-insecure-requests": '1',
+
+            "user-agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
             lo = session.post('https://free.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',cookies=cookies,headers=headers).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -711,21 +725,35 @@ def free1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header = ({'authority': 'free.facebook.com',
-            'method': 'GET',
-            'scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
+            header = ({"authority": 'free.facebook.com',
+
+            "method": 'GET',
+
+            "scheme": 'https',
+
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+            "accept-language": 'en-US,en;q=0.9',
+
+            "cache-control": 'max-age=0',
+
+            "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+
+            "sec-ch-ua-mobile": '?0',
+
+            "sec-ch-ua-platform": '"Windows"',
+
+            "sec-fetch-dest": 'document',
+
+            "sec-fetch-mode": 'navigate',
+
+            "sec-fetch-site": 'none',
+
+            "sec-fetch-user": '?1',
+
+            "upgrade-insecure-requests": '1',
+
+            "user-agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
             lo = session.post('https://free.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',cookies=cookies,headers=headers).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -821,21 +849,35 @@ def free1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header = ({'authority': 'free.facebook.com',
-            'method': 'GET',
-            'scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
+            header = ({"authority": 'free.facebook.com',
+
+            "method": 'GET',
+
+            "scheme": 'https',
+
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+            "accept-language": 'en-US,en;q=0.9',
+
+            "cache-control": 'max-age=0',
+
+            "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+
+            "sec-ch-ua-mobile": '?0',
+
+            "sec-ch-ua-platform": '"Windows"',
+
+            "sec-fetch-dest": 'document',
+
+            "sec-fetch-mode": 'navigate',
+
+            "sec-fetch-site": 'none',
+
+            "sec-fetch-user": '?1',
+
+            "upgrade-insecure-requests": '1',
+
+            "user-agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',})
             lo = session.post('https://free.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',cookies=cookies,headers=headers).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
